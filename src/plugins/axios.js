773c77fc49ffcd61d274d1fefe80f058bc4c1171
Roland1993 <roland.buzzooka@gmail.com>
2018-07-13 19:27:04 +0200

    Axios instance setup

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from 'vue';
 import axios from "axios";
+import store from '../store/store'
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -9,50 +10,39 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
+  baseURL: 'http://localhost:8000', //process.env.baseURL || process.env.apiUrl || ""
+  timeout: 5000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
 
-_axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
+//request interceptor
+_axios.interceptors.request.use(config => {
+  const token = store.state.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
   }
-);
+  return config
+}, error => {
+  return Promise.reject(error)
+});
 
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
+// response interceptor
+_axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  //Here we can manage the error response
+})
+
 
 Plugin.install = function(Vue, options) {
   Vue.axios = _axios;
   window.axios = _axios;
   Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
+    $http: {
+      get: () => _axios
+    }
   });
 };
 
