2685430a4c3e2e41d5d7dac0dda71ac1f9c6ebd1
klentgary <klent.buzzooka@gmail.com>
2018-07-15 02:46:48 +0800

    Added Sidebar Titles

---
+++ src/components/DashboardSidebar/DashboardSidebar.js
@@ -0,0 +1,24 @@
+export default {
+    data () {
+        return {
+          items: [
+            { title: 'Dashboard', icon: '@/assets/logo/buzzooka-white.png' },
+            { title: 'Projects' },
+            { title: 'Clients' },
+            { title: 'Calendar' },
+            { title: 'Milestones' },
+            { title: 'Forms' },
+            { title: 'Invoice' },
+            { title: 'Payment' },
+            { title: 'Timers' }, 
+            { title: 'Cloud' }, 
+            { title: 'Team' }, 
+            { title: 'Chat' }, 
+            { title: 'Reports' }, 
+            { title: 'Support' }, 
+            { title: 'Services' }, 
+            { title: 'Logout' }, 
+          ],
+        }
+    }
+  }
--- src/components/DashboardSidebar/DashboardSidebar.vue
+++ src/components/DashboardSidebar/DashboardSidebar.vue
@@ -2,12 +2,29 @@
     <div class="sidebar">
         <div class="s__title">
             <div class="t__logo">
-                <img class="responsive-img" src="@/assets/logo/buzzooka-blue.png" alt="">
+                <img class="responsive-img" src="@/assets/logo/buzzooka-white.png" alt="">
             </div>
             <div class="t__hamburger">Hamburger Here</div>
         </div>
-        <div class="s__items">ITEMS HERE</div>
+
+        <v-divider></v-divider>
+
+        <v-list class="s__items">
+            <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+            >
+                <v-list-tile-action>
+                    <img :src="item.icon" />
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
     </div>
 </template>
+
 <script src="./DashboardSidebar.js"></script>
 <style lang="scss" scoped src="./DashboardSidebar.scss"></style>
