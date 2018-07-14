2ca80e7d29b5b64682ecb1331ea471a8aaffe48a
Roland1993 <roland.buzzooka@gmail.com>
2018-07-14 13:47:09 +0200

    Make some fixes on layout

--- src/components/DashboardSidebar/DashboardSidebar.vue
+++ src/components/DashboardSidebar/DashboardSidebar.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="sidebar">
-        <div class="s__title">
-            <div class="t__logo">Logo</div>
-            <div class="t__hamburger">Ham</div>
-        </div>
-        <div class="s__items"></div>
+        <!--<div class="s__title">-->
+            <!--<div class="t__logo">LOGO HERE</div>-->
+            <!--<div class="t__hamburger">Hamburger Here</div>-->
+        <!--</div>-->
+        <!--<div class="s__items"></div>-->
     </div>
 </template>
 <script src="./DashboardSidebar.js"></script>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home/Home.vue'
 import Login from './views/Login/Login.vue'
 import Dashboard from './views/Dashboard/Dashboard.vue'
@@ -7,24 +8,24 @@ import Dashboard from './views/Dashboard/Dashboard.vue'
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: '/login',
-            name: 'login',
-            component: Login
-        },
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
 
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
 
-        {
-            path: '/dashboard',
-            name: 'dashboard',
-            component: Dashboard
-        }
-    ],
-    mode: 'history'
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    }
+  ],
+  mode: 'history'
 })
