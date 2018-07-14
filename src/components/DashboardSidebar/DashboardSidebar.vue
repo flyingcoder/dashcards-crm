e7af22b2b4cd443176abdd8bca3ce1de8e750457
Roland1993 <roland.buzzooka@gmail.com>
2018-07-14 15:06:26 +0200

    Adding Buzooka logo

--- src/components/DashboardSidebar/DashboardSidebar.vue
+++ src/components/DashboardSidebar/DashboardSidebar.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="sidebar">
         <div class="s__title">
-            <div class="t__logo">LOGO HERE</div>
+            <div class="t__logo">
+                <img class="responsive-img" src="@/assets/logo/buzzooka-blue.png" alt="">
+            </div>
             <div class="t__hamburger">Hamburger Here</div>
         </div>
         <div class="s__items">ITEMS HERE</div>
