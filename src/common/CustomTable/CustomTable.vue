316ac75332b12524d73872a8c8c516ff24560d29
Roland1993 <roland.buzzooka@gmail.com>
2018-07-31 19:02:27 +0200

    Finish service table

--- src/common/CustomTable/CustomTable.vue
+++ src/common/CustomTable/CustomTable.vue
@@ -72,32 +72,6 @@
 
   </v-data-table>
 
-
-
-
-  <!--<v-data-table-->
-          <!--v-bind="$attrs"-->
-          <!--:pagination.sync="pagination"-->
-  <!--&gt;-->
-
-    <!--<v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear> &lt;!&ndash; LOADING  PROGRESS BAR&ndash;&gt;-->
-
-    <!--<template slot="headers" slot-scope="props">-->
-      <!--<slot name="headers"></slot>-->
-    <!--</template>-->
-
-    <!--<template slot="items" slot-scope="props">-->
-      <!--<slot name="items" :items="props"></slot>-->
-    <!--</template>-->
-
-    <!--<template slot="no-data">-->
-      <!--<v-alert :value="true" color="error" icon="warning">-->
-        <!--Sorry, nothing to display here :(-->
-        <!--<pre>{{$props}}</pre>-->
-      <!--</v-alert>-->
-    <!--</template>-->
-
-  <!--</v-data-table>-->
 </template>
 <script src="./CustomTable.js"></script>
 <style lang="scss" scoped src="./CustomTable.scss"></style>
