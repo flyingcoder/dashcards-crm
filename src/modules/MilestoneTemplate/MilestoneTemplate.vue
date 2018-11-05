6cb7ec59c4452c4116f68453b04c870dba12452b
Roland doda <rolanddoda2014@gmail.com>
2018-11-05 16:51:46 +0000

    Merged in bugfix-rightWay/templates-page-is-broken (pull request #115)
    Fix templates page console errors and make it stable

--- src/modules/MilestoneTemplate/MilestoneTemplate.vue
+++ src/modules/MilestoneTemplate/MilestoneTemplate.vue
@@ -39,10 +39,12 @@
 					<custom-table
 							:headers="headers"
 							:items="items"
-							:total-items="total_items"
 							:loading="loading"
-							has-checkbox
-							has-header-icon
+							:sort="sort"
+							:has-checkbox="true"
+							hide-actions
+							@items-selected="selected_ids = $event"
+							@sorted="changeSort"
 					>
 						<template slot="custom-item" slot-scope="item"> <!-- Table Items -->
 							<td class="text-xs-center">{{ item.item.name }}</td>
@@ -63,6 +65,30 @@
 
 							</td>
 						</template>
+
+						<template slot="table-actions">
+
+							<div class="actions-wrapper">
+
+								<div class="bulk-delete">
+									<v-btn color="indigo" dark outline :disabled="!show_delete_selected">
+										Delete Selected
+									</v-btn>
+								</div>
+
+								<div class="rows-per-page-dropdown">
+									Rows per page: <v-select :items="rows_per_page_items" menu-props="auto" v-model="rows_per_page"></v-select>
+								</div>
+
+								<div class="pagination">
+									<div class="text-xs-center pt-2">
+										<v-pagination :length="total_items" :total-visible="5" v-model="page"></v-pagination>
+									</div>
+								</div>
+
+							</div>
+						</template>
+
 					</custom-table>
 				</v-flex>
 			</v-layout>
@@ -74,6 +100,8 @@
 <script src="./MilestoneTemplate.js"></script>
 
 <style lang="scss" scoped>
+	@import '~@/sass/_variables';
+
 	.milestone-template {
 
 		.cell-with-icon {
@@ -90,6 +118,8 @@
 				}
 			}
 		}
+
+		@include customTableRow; //css used for styling the last row of the table
 
 	}
 </style>
