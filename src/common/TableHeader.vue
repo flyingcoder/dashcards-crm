0a0b2554fbf7e458c98951079938a53157a012bb
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2018-11-09 18:16:35 +0000

    Merged in feature-suitable/redesign-the-add-new-button-to-make-it-more-user-friendly (pull request #130)
    Redesign the add new button to make it more user friendly
    
    * Redesign the add new button to make it more user friendly
    
    * fix errors

--- src/common/TableHeader.vue
+++ src/common/TableHeader.vue
@@ -1,42 +1,36 @@
-<template functional>
-	<div class="table-header">
-		<component :is="injections.components.Breadcrumb" :paths="props.paths"/>
-		
-		<v-speed-dial v-model="fab" direction="left" hover   slide-y-reverse-transition>
-        <v-btn slot="activator" v-model="fab" class="more__btn" fab small>
-          <v-icon>more_horiz</v-icon>
-        </v-btn>
-        <v-btn fab dark small class="add__btn" v-on="listeners">
-          <v-icon>add</v-icon>
-        </v-btn>
-      </v-speed-dial>
-	</div>
+<template>
+    <div class="table-header">
+        <breadcrumb :paths="paths"/>
+        
+        <v-speed-dial direction="left" slide-y-reverse-transition >
+            <v-btn slot="activator" fab small class="add__btn" @click="$emit('click')">
+                <v-icon>add</v-icon>
+            </v-btn>
+            <v-btn slot="activator" class="more__btn" fab small>
+                <v-icon>more_horiz</v-icon>
+            </v-btn>
+        </v-speed-dial>
+    </div>
 </template>
 
 <script>
-	import Breadcrumb from '@/common/Breadcrumb.vue'
+    import Breadcrumb from '@/common/Breadcrumb.vue'
 
-	export default {
-		name: 'TableHeader',
-		functional: true,
-		props: {
-			paths: Array,
-		},
-		data (){
-			return{
-				fab: false,
-			}
-		},
-		inject: {
-			components: {
-				default: {
-					Breadcrumb
-				}
-			}
-		}
-	}
+    export default {
+        name: 'TableHeader',
+        props: {
+            paths: Array,
+        },
+        data (){
+            return{
+                fab: true,
+            }
+        },
+        components: {
+            Breadcrumb
+        }
+    }
 </script>
-
 <style lang="scss" scoped>
 	@import "~@/sass/variables";
 
