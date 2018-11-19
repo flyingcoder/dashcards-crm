f6b5e019a691303b74ebf73aafcdd7ebbf1fb35c
Roland1993 <roland.buzzooka@gmail.com>
2018-11-19 19:55:55 +0100

    Members Tab content: add members, detele functionality

--- src/modules/ProjectPreview/components/Members/Members.js
+++ src/modules/ProjectPreview/components/Members/Members.js
@@ -1,31 +1,54 @@
 import CustomTable from '@/common/CustomTable/CustomTable.vue'
+import DeleteDialog from '@/common/DeleteDialog.vue'
+import AddDialog from './AddDialog/AddDialog.vue'
 import { table_functionality } from "@/services/table-functionality";
 
 export default {
 	name: 'Members',
 	mixins: [table_functionality],
 	components: {
-		CustomTable
+		CustomTable, AddDialog, DeleteDialog
+	},
+	inheritAttrs: false,
+
+	props: {
+		id: [Number, String]
 	},
 
 	data: () =>({
+		add_dialog: false,
 		headers: [
 			{text: 'Member', value: 'member'},
+			{text: 'Email', value: 'email'},
+			{text: 'Telephone', value: 'telephone'},
 			{text: 'Position', value: 'position'},
-			{text: 'Location', value: 'location'},
-			{text: 'Total Hours', value: 'total_hours'},
+			{text: 'Tasks', value: 'tasks'},
 			{id: 4, is_action: true},
 		],
 		sortList: [
 			{ title: 'Sort by member', },
+			{ title: 'Sort by email', },
+			{ title: 'Sort by telephone', },
 			{ title: 'Sort by position', },
-			{ title: 'Sort by location', },
-			{ title: 'Sort by total hours', },
-			{ title: 'Sort by total amount', }
+			{ title: 'Sort by tasks', }
 		],
+		table_config: {
+			route_name: 'project_preview',
+			add_message: 'New Members(s) added successfully!',
+			delete_message: 'Members deleted successfully!',
+			refresh_table_message: 'Table refreshed',
+			refresh_table_api_name: 'paginate_tab_members_table'
+		},
 	}),
+
+	computed: {
+		dynamic_api() {
+			return `api/projects/${this.id}/member`
+		}
+	},
 
 	created() {
 		this.$router.replace({ name: 'project_preview', query: { tab: 'Members' }})
+		this.fill_table('get_members', true, this.dynamic_api)
 	},
 }
