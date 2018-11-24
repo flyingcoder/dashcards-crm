0ec867793500a3735bee870171548c3b8888e4d6
Roland doda <rolanddoda2014@gmail.com>
2018-11-24 15:04:53 +0000

    Merged in feature-rightWay/add-tasks-inside-boxes-in-milestone-tab (pull request #187)
    Add tasks inside dynamic boxes inside milestones tab
    
    Approved-by: Roland doda <rolanddoda2014@gmail.com>

--- src/modules/ProjectPreview/components/MilestonesTab/DynamicBox/DynamicBox.vue
+++ src/modules/ProjectPreview/components/MilestonesTab/DynamicBox/DynamicBox.vue
@@ -11,7 +11,11 @@
 					</v-btn>
 				</v-flex>
 			</template>
-			<div class="content" slot="content"></div>
+			<div class="content" slot="content">
+				<div class="task" v-for="task of box.tasks" :key="task.id">
+					{{ task.title }} 󠁿󠁿󠁿<span class="remove-icon">🗙</span>
+				</div>
+			</div>
 			<v-btn large slot="footer" round class="add__new_btn" color="#3b589e" dark>
 				ADD NEW
 			</v-btn>
@@ -40,6 +44,7 @@
 </script>
 <style lang="scss" scoped>
 @import '~@/sass/_variables';
+
 	.dynamic-card {
 		.action{
 			background-color: $tableBlueBg;
@@ -48,9 +53,32 @@
 				color: $tableTitleBlue;
 			}
 		}
+
+		@include styledScrollFor('.content');
+
 		.content {
 			height: 400px;
 			border: 1px solid $tableBorderBlue;
+			padding: 0;
+			overflow: auto;
+
+			.task {
+				padding: 15px;
+				background: gainsboro;
+				width: 100%;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				border-bottom: 1px solid brown;
+
+				.remove-icon {
+					cursor: pointer;
+					font-size: 1.3em;
+					color: #a06060;
+				}
+
+			}
+
 		}
 		.add__new_btn{
 			font-size: 12px;
