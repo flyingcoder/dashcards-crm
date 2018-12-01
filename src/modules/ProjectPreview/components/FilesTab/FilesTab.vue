555576dce81b7328312830cf232a6e2d25e0c416
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2018-12-01 09:27:10 +0000

    Merged in suitable/fix-the-design-of-files-tab-page (pull request #224)
    Fix the design of files tab page

--- src/modules/ProjectPreview/components/FilesTab/FilesTab.vue
+++ src/modules/ProjectPreview/components/FilesTab/FilesTab.vue
@@ -1,36 +1,34 @@
 <template>
     <div class="files__tab">
         <div class="drop__files">
-            <vue-dropzone
-                ref="dropzone"
-                :duplicateCheck="true"
-                id="dropzone"
-                :options="dropzoneOptions"
-                :useCustomSlot=true
-                dictFileTooBig="File too big"
-                dictInvalidFileType="Invalid file type"
-                @vdropzone-success="file_added"
-            >
 	            <div class="drop__files_body">
-		            <v-layout row align-center
-		                      justify-center fill-height
-		                      class="drop__files_content"
-		            >
-			            <v-flex xs4 class="text-xs-center">
-				            <div class="file__icon">
-					            <v-icon>file_copy</v-icon>
-				            </div>
+                    <vue-dropzone
+                        ref="dropzone"
+                        :duplicateCheck="true"
+                        id="dropzone"
+                        :options="dropzoneOptions"
+                        :useCustomSlot=true
+                        dictFileTooBig="File too big"
+                        dictInvalidFileType="Invalid file type"
+                        @vdropzone-success="file_added"
+                    >
+                            <v-layout row align-center
+                                    justify-center fill-height
+                                    class="drop__files_content"
+                            >
+                                <div class="file__icon">
+                                    <v-icon>file_copy</v-icon>
+                                </div>
 
-				            <div class="drop__title">Drop files here</div>
-				            <div class="drop__text">or</div>
-				            <div class="drop__btn">
-					            <v-btn large dark color="#3b589e">Choose your files</v-btn>
-				            </div>
-
-			            </v-flex>
-		            </v-layout>
+                                <div class="drop__title">Drop files here</div>
+                                <div class="drop__text">or</div>
+                                <div class="drop__btn">
+                                    <v-btn large dark color="#3b589e">Choose your files</v-btn>
+                                </div>
+                                
+                            </v-layout>
+                    </vue-dropzone>
 	            </div>
-            </vue-dropzone>
         </div>
         
         <custom-table
@@ -44,7 +42,7 @@
 
             <template slot="toolbar">
                 <v-layout row class="file__list_header">
-                    <v-flex md10 sm8 xs7 class="file__tab">
+                    <v-flex md10 sm12 xs12 class="file__tab">
                         <v-btn color="#b3b7c3" flat medium>
                             <v-icon dark left class="all">select_all</v-icon>All
                         </v-btn>
@@ -64,7 +62,7 @@
                             Add Link
                         </v-btn>
                     </v-flex>
-                    <v-flex md2 sm4 xs5 class="file__option text-xs-right">
+                    <v-flex md2 sm12 xs12 class="file__option text-xs-right">
                         <v-btn fab flat small class="list__view">
                             <v-icon>list</v-icon>
                         </v-btn>
