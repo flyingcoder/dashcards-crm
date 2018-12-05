50e78fd4bca018a41143cb2f129eddd9e1069c63
Roland doda <rolanddoda2014@gmail.com>
2018-12-05 15:35:46 +0000

    Merged in feature-rightWay/notify-user-when-is-idle-for-30-mins (pull request #244)
    Notify user when is idle for 30 mins
    
    Approved-by: Roland doda <rolanddoda2014@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,52 @@
 <script>
 
 import snackbar from './common/snackbar/snackbar.vue'
+
 export default {
   name: 'App',
   components: {snackbar},
   data: () => ({
+    user_moved_mouse: 0
+  }),
 
-  })
+  created() {
+	  // Let's check if the browser supports notifications
+	  if (!("Notification" in window)) {
+		  alert("This browser does not support desktop notification");
+	  }else if (Notification.permission !== "denied") {
+	  	this.request_notification_permission()
+	  }
+  },
+
+  watch: {
+  	user_moved_mouse(val) {
+  		if (val >= 180) { //if user is idle for 30 mins
+			  let notification = new Notification("You were idle for 30 mins")
+			  setTimeout(() => {
+				  notification.close();
+			  }, 4500);
+      }
+    }
+  },
+
+  mounted() {
+  	document.addEventListener('mousemove', this.reset_timer)
+    setInterval(() => {
+    	this.user_moved_mouse += 10
+    }, 10000)
+  },
+
+  methods: {
+  	reset_timer() {
+  		this.user_moved_mouse = 0
+    },
+    request_notification_permission() {
+	    Notification.requestPermission().then(function (permission) {
+		    if (permission === "denied") {
+			    this.request_notification_permission()
+		    }
+	    });
+    }
+  }
 }
 </script>
