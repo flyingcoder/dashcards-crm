ce9a78cee816c14c9c2ed3d0ac36e092ff978b4b
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2018-12-11 22:02:04 +0800

    Fix the design of invoice dashcard in project hq

--- src/modules/ProjectPreview/components/HQTab/InvoiceTable/InvoiceTable.vue
+++ src/modules/ProjectPreview/components/HQTab/InvoiceTable/InvoiceTable.vue
@@ -1,50 +1,53 @@
 <template>
-	<div class="invoice-table">
+	<v-flex md6 xs12 class="invoice-card">
+		<div class="invoice__content">
+			<dash-card>
+				<div slot="actions"></div>
+				<div class="content-wrapper" slot="content">
 
-		<dash-card>
-			<div slot="actions"></div>
-			<div class="content-wrapper" slot="content">
+					<custom-table
+							:headers="headers"
+							:items="items"
+							:loading="loading"
+							:has-checkbox="true"
+							hide-actions
+							@items-selected="selected_ids = $event"
+					>
 
-				<custom-table
-						:headers="headers"
-						:items="items"
-						:loading="loading"
-						:has-checkbox="true"
-						hide-actions
-						@items-selected="selected_ids = $event"
-				>
+						<div class="toolbar" slot="toolbar">
+							<v-flex xs4>
+								<v-select :items="['Remind', 'Cancel', 'Archive']" label="Batch Actions"></v-select>
+							</v-flex>
+						</div>
 
-					<div class="toolbar" slot="toolbar">
-						<v-flex xs4>
-							<v-select :items="['Remind', 'Cancel', 'Archive']" label="Batch Actions"></v-select>
-						</v-flex>
-					</div>
+						<template slot="custom-item" slot-scope="item">
+							<td>{{ item.item.due_date }}</td>
+							<td>{{ item.item.invoice_id }}</td>
+							<td>{{ item.item.bill_to }}</td>
+							<td>{{ item.item.total_amount }}</td>
+							<td>
+								<v-select
+										:items="['Copy', 'Print', 'PDF', 'Share link']"
+										label="Select action"
+								></v-select>
+							</td>
+						</template>
 
-					<template slot="custom-item" slot-scope="item">
-						<td>{{ item.item.due_date }}</td>
-						<td>{{ item.item.invoice_id }}</td>
-						<td>{{ item.item.bill_to }}</td>
-						<td>{{ item.item.total_amount }}</td>
-						<td>
-							<v-select
-									:items="['Copy', 'Print', 'PDF', 'Share link']"
-									label="Select action"
-							></v-select>
-						</td>
-					</template>
+					</custom-table>
 
-				</custom-table>
-
-			</div>
-		</dash-card>
-
-	</div>
+				</div>
+			</dash-card>
+		</div>
+	</v-flex>
 </template>
 
 <script src="./InvoiceTable.js">
 </script>
-<style lang="scss" scoped>
-.invoice-table {
-  width: 100%;
+<style lang="scss" scoped src="./InvoiceTable.scss">
+</style>
+
+<style scoped>
+>>> .content__wrapper .buzz__tables {
+  all: unset;
 }
 </style>
