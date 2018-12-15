e4a1852bdb3365fc38b5a816185416b11981ff6c
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2018-12-15 09:37:27 +0000

    Merged in suitable-rightway/design-add-and-edit-milestone-modal (pull request #284)
    Design add and edit milestone modal

--- src/modules/Milestone/components/MilestoneDialog/MilestoneDialog.vue
+++ src/modules/Milestone/components/MilestoneDialog/MilestoneDialog.vue
@@ -2,36 +2,51 @@
 	<div class="milestone-dialog">
 		<v-layout row justify-center>
 			<v-dialog v-model="open" persistent max-width="600px">
-				<v-card>
+				<v-card class="milestone__dialog">
 
-					<v-card-title>
-						<span class="headline">{{ dialogTitle }}</span>
+					<v-card-title class="dialog__header">
+						<span class="dialog__title">{{ dialogTitle }}</span>
+						<v-btn fab small class="close__dialog" @click="cancel">
+							<v-icon>close</v-icon>
+						</v-btn>
 					</v-card-title>
 
-					<v-card-text>
+					<v-card-text class="dialog__body">
 						<v-container grid-list-md>
 							<v-layout wrap>
 
 								<v-flex xs12 sm6>
 									<v-text-field
+											class="dialog__textfield"
 											label="Add milestone title"
 											v-model.trim="title"
+											solo
+											hide-details
+											color="#657186"
 									></v-text-field>
 								</v-flex>
 
 								<v-flex xs12 sm6>
 									<v-select
+											class="dialog__selectfield"
 											label="Select Status"
 											v-model.trim="status"
 											:items="['Active', 'Inactive']"
+											solo
+											hide-details
+											color="#657186"
 									></v-select>
 								</v-flex>
 
 								<v-flex xs12 sm6>
 									<v-text-field
+											class="dialog__textfield"
 											label="Add days"
 											v-model.number="days"
 											type="number"
+											solo
+											hide-details
+											color="#657186"
 									></v-text-field>
 								</v-flex>
 
@@ -40,10 +55,9 @@
 
 					</v-card-text>
 
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn color="blue darken-1" flat @click="cancel">Cancel</v-btn>
-						<v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+					<v-card-actions class="dialog__actions">
+						<v-btn @click="cancel">Cancel</v-btn>
+						<v-btn @click="save">Save</v-btn>
 					</v-card-actions>
 
 				</v-card>
@@ -54,8 +68,7 @@
 
 <script src="./MilestoneDialog.js">
 </script>
-
-<style lang="scss" scoped>
-.milestone-dialog {
-}
+<style lang="scss" scoped src="./MilestoneDialog.scss">
+</style>
+<style scoped src="./MilestoneDialog.css">
 </style>
