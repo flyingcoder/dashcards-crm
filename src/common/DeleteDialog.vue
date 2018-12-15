3e76838c8f26e3539080a897c130f3cbba97c4ed
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2018-12-15 09:39:04 +0000

    Merged in suitable-rightway/design-all-delete-modal (pull request #283)
    Design all delete modal

--- src/common/DeleteDialog.vue
+++ src/common/DeleteDialog.vue
@@ -2,20 +2,22 @@
 	<v-layout row justify-center>
 		<v-dialog v-model="dialog" persistent max-width="500px">
 
-			<v-card>
+			<v-card class="delete__dialog">
 
-				<v-card-title>
-					<span class="headline">{{ title }}</span>
+				<v-card-title class="dialog__header">
+					<span class="dialog__title">{{ title }}</span>
+					<v-btn fab small class="close__dialog" @click="cancel_clicked">
+						<v-icon>close</v-icon>
+					</v-btn>
 				</v-card-title>
 
-				<v-card-text>
+				<v-card-text class="dialog__body">
 					{{ textContent }}
 				</v-card-text>
 
-				<v-card-actions>
-					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" flat @click="cancel_clicked">{{cancelButtonText}}</v-btn>
-					<v-btn color="blue darken-1" flat @click="delete_clicked">{{deleteButtonText}}</v-btn>
+				<v-card-actions class="dialog__actions">
+					<v-btn @click="cancel_clicked">{{cancelButtonText}}</v-btn>
+					<v-btn @click="delete_clicked">{{deleteButtonText}}</v-btn>
 				</v-card-actions>
 
 			</v-card>
@@ -57,3 +59,39 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~@/sass/_variables';
+
+@include styledDialog('.delete__dialog');
+
+.delete__dialog{
+
+  .dialog__header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+    padding: 0;
+    
+    @include styledCloseDialog('.close__dialog');
+
+    @include styledTitleDialog('.dialog__title');
+  }
+
+  @include styledScrollFor('.dialog__body');
+
+  .dialog__body{
+    color: $tableDarkText;
+    font-size: 16px;
+  }
+  
+  @include actionButtonsDialog('.dialog__actions');
+
+}
+
+@media only screen and (max-width: 480px){
+  
+}
+
+</style>
