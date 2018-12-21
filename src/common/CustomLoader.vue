a7412c73d8d3f426e66357259e9e7d7c0d5130cc
Roland1993 <roland.buzzooka@gmail.com>
2018-12-21 20:06:05 +0100

    Create reusable custom loader

---
+++ src/common/CustomLoader.vue
@@ -0,0 +1,43 @@
+<template>
+	<transition name="fade">
+		<div class="custom-loader" v-show="should_show">
+			<v-progress-circular
+					indeterminate
+					color="primary"
+					:size="72"
+			></v-progress-circular>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+  computed: {
+    should_show() {
+      return this.$store.getters.custom_loader
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-loader {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 99999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(224, 224, 189, 0.57);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  transform: scale(0);
+}
+</style>
