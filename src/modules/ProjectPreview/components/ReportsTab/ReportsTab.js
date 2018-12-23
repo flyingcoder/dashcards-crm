f4f99ccdb1d78a23370b8d7d43baf24d3b3af576
Roland1993 <roland.buzzooka@gmail.com>
2018-12-23 14:58:57 +0100

    Display website in Reports Tab page

--- src/modules/ProjectPreview/components/ReportsTab/ReportsTab.js
+++ src/modules/ProjectPreview/components/ReportsTab/ReportsTab.js
@@ -1,10 +1,24 @@
 export default {
-  name: 'ReportsTab',
+  data: () => ({
+    iframe_src: null,
+    iframe_loading: false
+  }),
 
   created() {
     this.$router.replace({
       name: 'project_preview',
       query: { tab: 'Reports' }
     })
+  },
+
+  methods: {
+    add_content() {
+      this.$store.commit('set_custom_loader', true)
+      this.iframe_src =
+        'https://www.your-report.com/clients/au/5cornerdental.com/aakspr6973'
+    },
+    iframe_loaded() {
+      this.$store.commit('set_custom_loader', false)
+    }
   }
 }
--- src/modules/ProjectPreview/components/ReportsTab/ReportsTab.vue
+++ src/modules/ProjectPreview/components/ReportsTab/ReportsTab.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="reports__tab">
         <div class="reports__body">
-            <div class="reports__content">
+            <div class="reports__buttons">
                 <div class="reports__option">
 
-                    <v-btn large color="#3b589e" class="add__link">
+                    <v-btn large color="#3b589e" 
+                           class="add__link"
+                           @click="add_content"
+                    >
                         Add Link
                     </v-btn>
 
@@ -17,6 +20,14 @@
                     </v-btn>
 
                 </div>
+            </div>
+            <div class="reports__content" v-if="iframe_src">
+                <iframe src="https://www.your-report.com/clients/au/5cornerdental.com/aakspr6973"
+                        frameborder="0"
+                        width="100%"
+                        height="500px"
+                        @load="iframe_loaded"
+                ></iframe>
             </div>
         </div>
     </div>
