75a694c1ad626b7e32328b61488cbc32765d3311
Roland1993 <roland.buzzooka@gmail.com>
2018-12-30 16:59:15 +0100

    Add auth guard using vue-router

--- src/router/routes/pricing.js
+++ src/router/routes/pricing.js
@@ -1,14 +1,5 @@
-import { check_user } from '../route_guard'
-
 export const pricing = {
   path: '/pricing',
   name: 'pricing',
-  component: () => import('@/modules/Pricing/Pricing.vue'),
-  beforeEnter(to, from, next) {
-    if (check_user.is_user_logged()) {
-      next()
-    } else {
-      next({ name: 'not_found' })
-    }
-  }
+  component: () => import('@/modules/Pricing/Pricing.vue')
 }
