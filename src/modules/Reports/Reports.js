81ce5a413e719f3abae8a46f81d3f63e23ef6b65
Roland1993 <roland.buzzooka@gmail.com>
2019-01-03 15:02:59 +0100

    Create Reports Page

---
+++ src/modules/Reports/Reports.js
@@ -0,0 +1,65 @@
+//TODO DRY, uses the same code as ReportsTab
+import makeRequestTo from '@/services/makeRequestTo'
+//Components
+import CustomDialog from '@/common/BaseComponents/CustomDialog/CustomDialog.vue'
+
+export default {
+  components: {
+    CustomDialog
+  },
+
+  props: {
+    id: [Number, String]
+  },
+
+  data: () => ({
+    iframe_src: null,
+    iframe_loading: false,
+    link: '',
+    title: '',
+    valid_url: false,
+    activate_save: false
+  }),
+
+  computed: {
+    is_disabled() {
+      return !this.link || !this.valid_url || !this.title
+    }
+  },
+
+  methods: {
+    open_dialog() {
+      this.$refs.dialog.open_dialog()
+    },
+
+    iframe_loaded() {
+      this.$store.commit('set_custom_loader', false)
+      this.activate_save = true
+    },
+
+    validate_url(event) {
+      this.$nextTick(() => {
+        this.valid_url = event.target.validity.valid
+      })
+    },
+
+    on_dialog_save() {
+      this.$refs.dialog.close_dialog()
+      this.$store.commit('set_custom_loader', false)
+      this.iframe_src = this.link
+    },
+
+    save_report() {
+      makeRequestTo
+        .add_new_report(this.id, {
+          url: this.link,
+          title: this.title
+        })
+        .then(() => {
+          this.link = ''
+          this.title = ''
+          this.activate_save = false
+        })
+    }
+  }
+}
