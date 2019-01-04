4cc18612e00c30f8929361c9a134db108901f733
Roland1993 <roland.buzzooka@gmail.com>
2019-01-04 15:03:04 +0100

    Fetch online users

--- src/store/store.js
+++ src/store/store.js
@@ -11,19 +11,22 @@ export default new Vuex.Store({
     snackbar: {
       status: false,
       message: ''
-    }
+    },
+    online_users: []
   },
   getters: {
     custom_loader: state => state.custom_loader,
     is_user_logged: state => !!state.user,
     user: state => state.user,
-    snackbar: state => state.snackbar
+    snackbar: state => state.snackbar,
+    online_users: state => state.online_users
   },
   mutations: {
     set_user: (state, payload) => (state.user = payload),
     remove_user: state => (state.user = null),
     open_snackbar: (state, payload) => (state.snackbar = payload),
-    set_custom_loader: (state, payload) => (state.custom_loader = payload)
+    set_custom_loader: (state, payload) => (state.custom_loader = payload),
+    set_online_users: (state, payload) => (state.online_users = payload)
   },
   actions: {
     login({ commit }, payload) {
