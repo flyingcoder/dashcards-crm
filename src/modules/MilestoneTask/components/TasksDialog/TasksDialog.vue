beefe5f415ccea56747d38b51201235dd3c4bdd2
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-01-04 09:23:51 +0000

    Merged in suitable-rightway/design-the-add-task-modal (pull request #346)
    Design the add task modal.

--- src/modules/MilestoneTask/components/TasksDialog/TasksDialog.vue
+++ src/modules/MilestoneTask/components/TasksDialog/TasksDialog.vue
@@ -2,66 +2,85 @@
 	<div class="tasks-dialog">
 		<v-layout row justify-center>
 			<v-dialog v-model="open" persistent max-width="600px">
-				<v-card>
+				<v-card class="task__dialog">
 
-					<v-card-title>
-						<span class="headline">{{ dialogTitle }}</span>
+					<v-card-title class="dialog__header">
+						<span class="dialog__title">{{ dialogTitle }}</span>
+						<v-btn small fab @click="cancel" class="close__dialog">
+							<v-icon dark>close</v-icon>
+						</v-btn>
 					</v-card-title>
 
-					<v-card-text>
-						<v-container grid-list-md>
-							<v-layout wrap>
+					<v-card-text class="dialog__body">
+						<v-layout wrap>
 
-								<v-flex xs12>
-									<v-text-field
-											label="Add task title"
-											v-model.trim="title"
-									></v-text-field>
-								</v-flex>
+							<v-flex xs12>
+								<v-text-field
+										class="dialog__textfield"
+										label="Add task title"
+										v-model.trim="title"
+										color="#667187"
+										solo
+										hide-details
+								></v-text-field>
+							</v-flex>
 
-								<v-flex xs5>
-									<v-select
-											label="Select Group"
-											v-model.trim="selected_group"
-											:items="group_items"
-											item-text="name"
-											item-value="id"
-									></v-select>
-								</v-flex>
+							<v-flex xs5>
+								<v-select
+										class="dialog__selectfield"
+										label="Select Group"
+										v-model.trim="selected_group"
+										:items="group_items"
+										item-text="name"
+										item-value="id"
+										color="#667187"
+										solo
+										hide-details
+								></v-select>
+							</v-flex>
 
-								<v-flex xs5>
-									<v-select
-											label="Select Status"
-											v-model.trim="status"
-											:items="['Complete', 'Pending', 'Behind', 'Open']"
-									></v-select>
-								</v-flex>
+							<v-flex xs5>
+								<v-select
+										class="dialog__selectfield"
+										label="Select Status"
+										v-model.trim="status"
+										:items="['Complete', 'Pending', 'Behind', 'Open']"
+										color="#667187"
+										solo
+										hide-details
+								></v-select>
+							</v-flex>
 
-								<v-flex xs2>
-									<v-text-field
-											label="Add days"
-											v-model.number="days"
-											type="number"
-											min="0"
-									></v-text-field>
-								</v-flex>
+							<v-flex xs2>
+								<v-text-field
+										class="dialog__textfield"
+										label="Add days"
+										v-model.number="days"
+										type="number"
+										min="0"
+										color="#667187"
+										solo
+										hide-details
+								></v-text-field>
+							</v-flex>
 
-								<v-flex xs12>
-									<v-textarea
-											label="Add task description"
-											v-model.trim="description"
-									></v-textarea>
-								</v-flex>
+							<v-flex xs12>
+								<v-textarea
+										class="dialog__textarea"
+										label="Add task description"
+										v-model.trim="description"
+										color="#667187"
+										solo
+										hide-details
+								></v-textarea>
+							</v-flex>
 
-							</v-layout>
-						</v-container>
-
+						</v-layout>
 					</v-card-text>
 
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn color="blue darken-1" flat @click="cancel">Cancel</v-btn>
-						<v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+					<v-card-actions class="dialog__actions">
+						<v-btn @click="cancel">Cancel</v-btn>
+						<v-btn @click="save">Save</v-btn>
 					</v-card-actions>
 
 				</v-card>
@@ -72,8 +91,7 @@
 
 <script src="./TasksDialog.js">
 </script>
-
-<style lang="scss" scoped>
-.tasks-dialog {
-}
+<style lang="scss" scoped src="./TasksDialog.scss">
+</style>
+<style scoped src="./TasksDialog.css">
 </style>
