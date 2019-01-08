79a6e4120704ba03b214fcc71e09392aa304c4fa
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-01-08 20:05:33 +0800

    Design add new member modal in Teams Page

--- src/modules/Teams/components/TeamsDialog/TeamsDialog.vue
+++ src/modules/Teams/components/TeamsDialog/TeamsDialog.vue
@@ -1,20 +1,26 @@
 <template>
 	<v-layout row justify-center>
 		<v-dialog v-model="open" persistent max-width="600px">
-			<v-card>
+			<v-card class="teams-dialog">
 
-				<v-card-title>
-					<span class="headline">{{ title }}</span>
+				<v-card-title class="dialog__header">
+					<span class="dialog__title">{{ title }}</span>
+					<v-btn small fab @click="cancel" class="close__dialog">
+						<v-icon dark>close</v-icon>
+					</v-btn>
 				</v-card-title>
 
 				<v-progress-linear v-if="loading" slot="progress" color="blue" indeterminate></v-progress-linear>
 
-				<v-card-text v-show="!loading">
-					<v-container grid-list-md>
+				<v-card-text v-show="!loading" class="dialog__body">
 						<v-layout wrap>
 
 							<v-flex xs12 sm6>
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('first_name')"
 										:error="$v.first_name.$error"
 										label="First Name"
@@ -24,6 +30,10 @@
 
 							<v-flex xs12 sm6>
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('last_name')"
 										:error="$v.last_name.$error"
 										label="Last Name"
@@ -33,6 +43,10 @@
 
 							<v-flex xs12 sm6>
 								<v-select
+                  						class="dialog__selectfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('group_name')"
 										:error="$v.group_name.$error"
 										label="Group Name"
@@ -45,6 +59,10 @@
 
 							<v-flex xs12 sm6>
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('job_title')"
 										:error="$v.job_title.$error"
 										label="Job Title"
@@ -54,6 +72,10 @@
 
 							<v-flex xs12 sm6>
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('email')"
 										:error="$v.email.$error"
 										label="Email"
@@ -63,6 +85,10 @@
 
 							<v-flex xs12 sm6>
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('contact_number')"
 										:error="$v.contact_number.$error"
 										label="Contact Number"
@@ -73,6 +99,10 @@
 
 							<v-flex xs12 sm6 v-if="!isEditDialog">
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('password')"
 										:error="$v.password.$error"
 										:append-icon="show_password ? 'visibility_off' : 'visibility'"
@@ -85,6 +115,10 @@
 
 							<v-flex xs12 sm6 v-if="!isEditDialog">
 								<v-text-field
+                  						class="dialog__textfield"
+										color="#667187"
+										solo
+										hide-details
 										@blur="on_blur_field('repeat_password')"
 										:error="$v.repeat_password.$error"
 										:append-icon="show_repeat_password ? 'visibility_off' : 'visibility'"
@@ -96,17 +130,17 @@
 							</v-flex>
 
 						</v-layout>
-					</v-container>
 
-					<small>Password is at least 6 character with numbers and letters</small><br>
-					<small>All fields are required</small>
+					<div class="dialog-description">
+						<small>Password is at least 6 character with numbers and letters</small><br>
+						<small>All fields are required</small>
+					</div>
 
 				</v-card-text>
 
-				<v-card-actions v-show="!loading">
-					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" flat @click="cancel">Cancel</v-btn>
-					<v-btn color="blue darken-1" flat :disabled="$v.$invalid" @click="save">Save</v-btn>
+				<v-card-actions v-show="!loading" class="dialog__actions">
+					<v-btn @click="cancel">Cancel</v-btn>
+					<v-btn :disabled="$v.$invalid" @click="save">Save</v-btn>
 				</v-card-actions>
 
 			</v-card>
@@ -116,3 +150,7 @@
 
 <script src="./TeamsDialog.js">
 </script>
+<style scoped src="./TeamsDialog.css">
+</style>
+<style lang="scss" scoped src="./TeamsDialog.scss">
+</style>
