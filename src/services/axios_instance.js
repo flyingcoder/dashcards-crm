2c1595624c06391f6847e8fbaa47ccb8c1224b2f
alvin_buzzooka <alvin.buzzooka@gmail.com>
2019-01-09 19:03:35 +0000

    Merged in dashcard (pull request #397)
    Dashcard
    
    * Change api url for dashcard
    
    * Remove password on dashcards set up
    
    * Update api url

--- src/services/axios_instance.js
+++ src/services/axios_instance.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 import { auth } from '@/plugins/auth'
 
 const request = axios.create({
-  baseURL: 'https://api.dashcard.com',
+  baseURL: 'https://api.dashcards.com',
   timeout: 5000
 })
 
