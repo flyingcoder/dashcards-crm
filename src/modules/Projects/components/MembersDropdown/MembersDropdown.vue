cd5fd5af93a09a60610c51802aa082b517067ab4
Roland1993 <roland.buzzooka@gmail.com>
2019-01-11 14:17:38 +0100

    Add remove functionality to members dropdown on Add new Project dialog when clicking v-chip "x" icon

--- src/modules/Projects/components/MembersDropdown/MembersDropdown.vue
+++ src/modules/Projects/components/MembersDropdown/MembersDropdown.vue
@@ -23,6 +23,7 @@
 						outline
 						class="chip--select-multi"
             close
+						@input="remove_chip(data.index)"
 				>
 					<img :src="require('@/assets/temp/user.png')" width="30" height="30">
 					&nbsp;{{data.item.first_name}}
@@ -47,6 +48,7 @@
 
 <script>
 import debounce from 'lodash/debounce'
+import _cloneDeep from 'lodash/cloneDeep'
 import makeRequestTo from '@/services/makeRequestTo'
 
 export default {
@@ -91,6 +93,12 @@ export default {
 
     is_item_active(id) {
       return this.members.includes(id)
+    },
+
+    remove_chip(index) {
+      let members = _cloneDeep(this.members)
+      members.splice(index, 1)
+      this.$emit('update:members', members)
     }
   }
 }
