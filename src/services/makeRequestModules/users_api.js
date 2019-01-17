4bbc888b72ed6b4cce10fa9eb545f5f1f8babf38
Roland1993 <roland.buzzooka@gmail.com>
2019-01-17 18:27:19 +0100

    Add new logic for the friends-list (presence channel)

--- src/services/makeRequestModules/users_api.js
+++ src/services/makeRequestModules/users_api.js
@@ -1,7 +1,7 @@
 import request from '@/services/axios_instance'
 
 export const users_api = {
-  get_all_users() {
-    return request.get('api/company/members?all=true')
+  logout_user(id) {
+    return request.post(`/api/logout`, { user_id: id })
   }
 }
