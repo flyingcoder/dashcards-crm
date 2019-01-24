b0117163125c6759faa0c0ecd29039595bc7915b
Roland1993 <roland.buzzooka@gmail.com>
2019-01-24 18:49:00 +0100

    Refactor icons and create a base component for dash icons. Also finish modal for selecting dash items

--- src/store/modules/cards/mutations.js
+++ src/store/modules/cards/mutations.js
@@ -1,3 +1,4 @@
 export const mutations = {
-  set_cards: (state, payload) => (state.cards = payload)
+  set_cards: (state, payload) => (state.cards = payload),
+  set_dash_items: (state, payload) => (state.dash_items = payload)
 }
