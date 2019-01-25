67006ff11a83261bbde5837f1fbe2709e5eb7c7c
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-01-26 03:11:05 +0800

    Fix lower folder missing

---
+++ src/modules/MemberProfile/components/Lower/Lower.js
@@ -0,0 +1,11 @@
+import Left from './Left/Left.vue'
+import Right from './Right/Right.vue'
+
+export default {
+  name: 'ProfileLower',
+
+  components: {
+    Left,
+    Right
+  }
+}
---
+++ src/modules/MemberProfile/components/Lower/Lower.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="profile-lower">
+        <left />
+        <right />
+    </div>
+</template>
+
+<script src="./Lower.js">
+</script>
+<style lang="scss" scoped src="./Lower.scss">
+</style>
