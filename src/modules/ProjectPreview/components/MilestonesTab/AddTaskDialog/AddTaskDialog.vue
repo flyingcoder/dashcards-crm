8a48d9f2b74796400e66581ba31354216125b193
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-01-29 01:55:18 +0800

    Use CustomTable component

--- src/modules/ProjectPreview/components/MilestonesTab/AddTaskDialog/AddTaskDialog.vue
+++ src/modules/ProjectPreview/components/MilestonesTab/AddTaskDialog/AddTaskDialog.vue
@@ -1,82 +1,71 @@
 <template>
 	<div class="tasks-dialog">
-		<v-layout row justify-center>
-			<v-dialog v-model="open" persistent max-width="600px">
-				<v-card class="task-dialog">
+		<custom-dialog
+				ref="dialog"
+				:open.sync="dialog"
+				title="Add Task"
+		>
+			<template slot="content">
+				<v-layout wrap class="custom-dialog">
+					<v-flex xs12 md12>
+						<v-text-field
+								class="dialog__textfield"
+								label="Add task title"
+								v-model.trim="title"
+								solo
+								hide-details
+								color="#657186"
+						></v-text-field>
+					</v-flex>
 
-					<v-card-title class="dialog__header">
-						<span class="dialog__title">{{ dialogTitle }}</span>
-						<v-btn fab small class="close__dialog" @click="cancel">
-							<v-icon>close</v-icon>
-						</v-btn>
-					</v-card-title>
+					<v-flex xs12 md6>
+						<date-picker
+								class="dialog__date"
+								label="Start Date"
+								prepend-icon="event"
+								readonly
+								:value="start_date"
+								@input="start_date_changed"
+								:max="end_date"
+						/>
+					</v-flex>
 
-					<v-card-text class="dialog__body">
-						<v-layout wrap>
+					<v-flex xs12 md6>
+						<date-picker
+								class="dialog__date"
+								label="End Date"
+								prepend-icon="event"
+								readonly
+								:value="end_date"
+								@input="end_date_changed"
+								:min="start_date"
+						/>
+					</v-flex>
 
-							<v-flex xs12 md12>
-								<v-text-field
-										class="dialog__textfield"
-										label="Add task title"
-										v-model.trim="title"
-										solo
-										hide-details
-										color="#657186"
-								></v-text-field>
-							</v-flex>
+					<v-flex xs12>
+						<members-dropdown :members.sync="members.selected"
+											:member-items="members.items"
+											:is-loading="members.loading"
+											@search="filter_dropdown_items('members', $event)"
+											class="task-member"
+						/>
+					</v-flex>
 
-							<v-flex xs12 md6>
-								<date-picker
-										class="task-date"
-										label="Start Date"
-										prepend-icon="event"
-										readonly
-										:value="start_date"
-										@input="start_date_changed"
-										:max="end_date"
-								/>
-							</v-flex>
+					<v-flex xs12>
+						<rich-editor
+								placeholder="Description"
+								v-model="description"
+								class="task-description"
+						/>
+					</v-flex>
+				</v-layout>
+			</template>
 
-							<v-flex xs12 md6>
-								<date-picker
-										class="task-date"
-										label="End Date"
-										prepend-icon="event"
-										readonly
-										:value="end_date"
-										@input="end_date_changed"
-										:min="start_date"
-								/>
-							</v-flex>
+			<template slot="button2">
+				<v-btn @click="save">Save</v-btn>
+			</template>
 
-							<v-flex xs12>
-								<members-dropdown :members.sync="members.selected"
-													:member-items="members.items"
-													:is-loading="members.loading"
-													@search="filter_dropdown_items('members', $event)"
-													class="task-member"
-								/>
-							</v-flex>
-
-							<v-flex xs12>
-								<rich-editor
-										placeholder="Description"
-										v-model="description"
-										class="task-description"
-								/>
-							</v-flex>
-
-						</v-layout>
-					</v-card-text>
-
-					<v-card-actions class="dialog__actions">
-						<v-btn @click="cancel">Cancel</v-btn>
-						<v-btn @click="save">Save</v-btn>
-					</v-card-actions>
-
-				</v-card>
-			</v-dialog>
-		</v-layout>
+		</custom-dialog>
 	</div>
 </template>
 
