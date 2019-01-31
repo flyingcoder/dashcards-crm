86676e30a20eb96b7ac13b33f15ad0e031140ded
Roland1993 <roland.buzzooka@gmail.com>
2019-01-31 15:59:20 +0100

    Permissions for tiles

--- src/modules/DashboardContent/components/DashboardTiles/DashboardTiles.js
+++ src/modules/DashboardContent/components/DashboardTiles/DashboardTiles.js
@@ -8,50 +8,6 @@ export default {
     Tile
   },
   data: () => ({
-    tiles: [
-      {
-        title: 'Projects',
-        value: 'projects',
-        color: '#ed8564',
-        icon: require('@/assets/icons/sidebar/projects.svg'),
-        admin_only: false
-      },
-      {
-        title: 'Open Tasks',
-        value: 'tasks',
-        color: '#50b4aa',
-        icon: require('@/assets/icons/sidebar/templates.svg'),
-        admin_only: false
-      },
-      {
-        title: 'Calendar',
-        value: 'calendars',
-        color: '#a085d5',
-        icon: require('@/assets/icons/sidebar/calendar.svg'),
-        admin_only: false
-      },
-      {
-        title: 'Timer',
-        value: 'timer',
-        color: '#1fb868',
-        icon: require('@/assets/icons/sidebar/timers.svg'),
-        admin_only: false
-      },
-      {
-        title: 'Inbound',
-        value: 'inbound',
-        color: '#00a7e5',
-        icon: require('@/assets/icons/sidebar/templates.svg'),
-        admin_only: true
-      },
-      {
-        title: 'Outbound',
-        value: 'outbound',
-        color: '#ff7f7e',
-        icon: require('@/assets/icons/sidebar/templates.svg'),
-        admin_only: true
-      }
-    ],
     counters: null
   }),
 
@@ -59,12 +15,62 @@ export default {
     user() {
       return this.$store.getters.user
     },
-    filteredTiles() {
-      if (this.user.is_admin) return this.tiles
-
-      return this.tiles.filter(function(tile) {
-        return !tile.admin_only
-      })
+    tiles() {
+      if (!this.user) return []
+      const user = this.user
+      return [
+        {
+          title: 'Projects',
+          value: 'projects',
+          color: '#ed8564',
+          icon: require('@/assets/icons/sidebar/projects.svg'),
+          can_view: () =>
+            user.can.projects_own.view ||
+            user.can.projects.view ||
+            user.is_admin
+        },
+        {
+          title: 'Open Tasks',
+          value: 'tasks',
+          color: '#50b4aa',
+          icon: require('@/assets/icons/sidebar/templates.svg'),
+          admin_only: false,
+          can_view: () =>
+            user.can.tasks.view || user.can.tasks_own.view || user.is_admin
+        },
+        {
+          title: 'Calendar',
+          value: 'calendars',
+          color: '#a085d5',
+          icon: require('@/assets/icons/sidebar/calendar.svg'),
+          admin_only: false,
+          can_view: () =>
+            user.can.calendars.view || user.can.calendars.view || user.is_admin
+        },
+        {
+          title: 'Timer',
+          value: 'timer',
+          color: '#1fb868',
+          icon: require('@/assets/icons/sidebar/timers.svg'),
+          admin_only: false,
+          can_view: () =>
+            user.can.timers.view || user.can.timers.view || user.is_admin
+        },
+        {
+          title: 'Inbound',
+          value: 'inbound',
+          color: '#00a7e5',
+          icon: require('@/assets/icons/sidebar/templates.svg'),
+          can_view: () => user.is_admin
+        },
+        {
+          title: 'Outbound',
+          value: 'outbound',
+          color: '#ff7f7e',
+          icon: require('@/assets/icons/sidebar/templates.svg'),
+          can_view: () => user.is_admin
+        }
+      ]
     }
   },
 
