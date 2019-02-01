7f260daa569540bfe5f9d20235c2a53d43a6197d
Roland1993 <roland.buzzooka@gmail.com>
2019-02-01 20:43:42 +0100

    Delete the groups page

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,6 @@ import { forgotPassword } from './routes/forgotPassword'
 import { pricing } from './routes/pricing'
 import { dashboard } from './routes/dashboard'
 import { clients } from './routes/clients'
-import { groups } from './routes/groups'
 import { templates } from './routes/templates'
 import { milestones } from './routes/milestones'
 import { milestoneTasks } from './routes/milestoneTasks'
@@ -41,7 +40,6 @@ export default new Router({
       children: [
         dashboard, // path: '' => info the path is empty because is the default component for the parent
         clients, // path: /dashboard/clients
-        groups, // path: /dashboard/team/groups
         templates, // path: /dashboard/templates
         milestones, // path: /dashboard/templates/:id/milestone
         milestoneTasks, // path: /dashboard/templates/:id/milestone/:id2/task
