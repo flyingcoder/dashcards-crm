2715540be3787bfa9a43c08b5253ce549534b80c
Roland1993 <roland.buzzooka@gmail.com>
2019-02-04 18:55:56 +0100

    Custom table, provide actions and apply change to Projects Table

--- src/modules/Projects/Projects.vue
+++ src/modules/Projects/Projects.vue
@@ -36,10 +36,16 @@
 				@items-selected="selected_ids = $event"
 				toolbar-title="Projects"
 				@sorted="changeSort"
+				@edit="open_edit_dialog"
+				@delete="open_delete_dialog"
+				@view="navigate_to_view_project($event.id)"
 		>
 
 			<template slot="custom-item" slot-scope="item">
-				<td class="text-cap clickable-td" @click="navigate_to_view_project(item.item.id)">{{ item.item.service_name }} </td>
+				<td class="text-cap clickable-td"
+				    @click="navigate_to_view_project(item.item.id)">{{
+					item.item.service_name }}
+				</td>
 				<td class="text-cap">{{ item.item.client_name }}</td>
 				<td class="text-cap">{{ item.item.manager_name }}</td>
 				<td>{{ item.item.started_at | from_now }}</td>
@@ -52,47 +58,29 @@
 				</td>
 				<td>{{ item.item.total_time }}</td>
 				<td class="text-cap">{{ item.item.status }}</td>
+			</template>
 
-				<td class="text-xs-center table__actions">
-
-					<v-btn fab small flat depressed
-						@click="open_edit_dialog(item.item)"
-					>
-						<img src="@/assets/icons/groups/edit.svg" alt="">
-					</v-btn>
-
-					<v-btn fab small flat depressed
-						@click="open_delete_dialog(item.item)"
-					>
-						<img src="@/assets/icons/groups/delete.svg" alt="">
-					</v-btn>
-
-					<v-btn fab flat small depressed
-			       		@click="navigate_to_view_project(item.item.id)"
-					>
-						<v-icon>pageview</v-icon>
-					</v-btn>
-
-				</td>
-		</template>
-
-		<template slot="table-actions">
+			<template slot="table-actions">
 
 				<div class="actions-wrapper">
 
 					<div class="bulk-delete">
-						<v-btn color="indigo" dark outline :disabled="!show_delete_selected">
+						<v-btn color="indigo" dark outline
+						       :disabled="!show_delete_selected">
 							Delete Selected
 						</v-btn>
 					</div>
 
 					<div class="rows-per-page-dropdown">
-						Rows per page: <v-select :items="rows_per_page_items" menu-props="auto" v-model="rows_per_page"></v-select>
+						Rows per page:
+						<v-select :items="rows_per_page_items" menu-props="auto"
+						          v-model="rows_per_page"></v-select>
 					</div>
 
 					<div class="pagination" v-show="should_show_pagination">
 						<div class="text-xs-center pt-2">
-							<v-pagination :length="total_items" :total-visible="5" v-model="page"></v-pagination>
+							<v-pagination :length="total_items" :total-visible="5"
+							              v-model="page"></v-pagination>
 						</div>
 					</div>
 
@@ -105,11 +93,14 @@
 			<div class="empty-content">
 				<div class="empty-svg">
 					<svg viewBox="0 0 250 250">
-						<path d="M28 38l194 0c7,0 14,3 19,8 5,5 9,12 9,19l0 115c0,7 -4,14 -9,19 -5,5 -12,8 -19,8 -21,0 -39,0 -61,0 -3,0 -6,-2 -6,-6 0,-3 3,-6 6,-6l4 0 0 0 57 0c4,0 8,-2 10,-5 3,-2 5,-6 5,-10l0 -115c0,-4 -2,-8 -5,-10 -2,-3 -6,-5 -10,-5l-194 0c-4,0 -8,2 -11,5l0 0c-2,2 -4,6 -4,10l0 115c0,4 2,8 4,10l0 0c3,3 7,5 11,5l57 0 0 0 6 0c4,0 7,3 7,6 0,4 -3,6 -7,6 -22,0 -41,0 -63,0 -8,0 -15,-3 -20,-8l0 0c-5,-5 -8,-12 -8,-19l0 -115c0,-7 3,-14 8,-19l0 0c5,-5 12,-8 20,-8zm99 138c12,0 22,9 22,22 0,12 -10,22 -22,22 -12,0 -22,-10 -22,-22 0,-13 10,-22 22,-22zm-82 -69c4,0 8,3 10,6l64 0c2,-3 6,-6 10,-6 4,0 8,3 10,6l56 0c2,-3 6,-6 10,-6 6,0 11,6 11,12 0,6 -5,12 -11,12 -4,0 -8,-3 -10,-6l-56 0c-2,3 -6,6 -10,6 -4,0 -8,-3 -10,-6l-64 0c-2,3 -6,6 -10,6 -7,0 -12,-6 -12,-12 0,-6 5,-12 12,-12z"/>
+						<path
+								d="M28 38l194 0c7,0 14,3 19,8 5,5 9,12 9,19l0 115c0,7 -4,14 -9,19 -5,5 -12,8 -19,8 -21,0 -39,0 -61,0 -3,0 -6,-2 -6,-6 0,-3 3,-6 6,-6l4 0 0 0 57 0c4,0 8,-2 10,-5 3,-2 5,-6 5,-10l0 -115c0,-4 -2,-8 -5,-10 -2,-3 -6,-5 -10,-5l-194 0c-4,0 -8,2 -11,5l0 0c-2,2 -4,6 -4,10l0 115c0,4 2,8 4,10l0 0c3,3 7,5 11,5l57 0 0 0 6 0c4,0 7,3 7,6 0,4 -3,6 -7,6 -22,0 -41,0 -63,0 -8,0 -15,-3 -20,-8l0 0c-5,-5 -8,-12 -8,-19l0 -115c0,-7 3,-14 8,-19l0 0c5,-5 12,-8 20,-8zm99 138c12,0 22,9 22,22 0,12 -10,22 -22,22 -12,0 -22,-10 -22,-22 0,-13 10,-22 22,-22zm-82 -69c4,0 8,3 10,6l64 0c2,-3 6,-6 10,-6 4,0 8,3 10,6l56 0c2,-3 6,-6 10,-6 6,0 11,6 11,12 0,6 -5,12 -11,12 -4,0 -8,-3 -10,-6l-56 0c-2,3 -6,6 -10,6 -4,0 -8,-3 -10,-6l-64 0c-2,3 -6,6 -10,6 -7,0 -12,-6 -12,-12 0,-6 5,-12 12,-12z"/>
 					</svg>
 				</div>
 				<div class="empty-btn">
-					<v-btn large dark color="#3b589e" @click="add_dialog = true">Add Project</v-btn>
+					<v-btn large dark color="#3b589e" @click="add_dialog = true">Add
+						Project
+					</v-btn>
 				</div>
 			</div>
 		</div>
