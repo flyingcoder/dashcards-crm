7ac3e04ae6136f764b394302a53774dbbdb6cc89
Roland1993 <roland.buzzooka@gmail.com>
2019-02-06 19:00:23 +0100

    Save Note

--- src/modules/Notes/Notes.vue
+++ src/modules/Notes/Notes.vue
@@ -2,44 +2,7 @@
   <div class="notes">
     <table-header :paths="paths" @click="dialog = true" />
 
-    <CustomDialog
-      title="Add Notes"
-      :open.sync="dialog"
-      button2-text="Save"
-      @button1="dialog = false"
-    >
-      <template #content>
-        <v-layout wrap class="custom-dialog">
-          <v-flex xs12>
-            <v-text-field
-              v-model.trim="title"
-              label="Add Title"
-              class="project__title mb-2"
-              solo
-              hide-details
-            >
-            </v-text-field>
-          </v-flex>
-
-          <v-flex xs12 class="mb-2">
-            <v-textarea
-              v-model.trim="comment"
-              solo
-              label="Add Comment"
-              hide-details
-            ></v-textarea>
-          </v-flex>
-
-          <v-flex xs12>
-            <rich-editor
-              placeholder="Description"
-              v-model.trim="description"
-              class="task-description"
-            />
-          </v-flex>
-        </v-layout>
-      </template>
-    </CustomDialog>
+    <NotesDialog :dialog.sync="dialog" @save="api_to.add_new_note" />
 
     <div class="notes-wrapper">
       <div class="notes-content">
