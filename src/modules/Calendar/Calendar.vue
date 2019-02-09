1a3dca0aa708903989903a85fb895df6c14684f4
Roland1993 <roland.buzzooka@gmail.com>
2019-02-09 19:32:29 +0100

    Add VCalendar component

--- src/modules/Calendar/Calendar.vue
+++ src/modules/Calendar/Calendar.vue
@@ -1,9 +1,34 @@
 <template>
   <div class="calendar">
     <table-header :paths="paths" :noButton="true" />
+    <div class="wrapper">
+      <div class="calendar-actions">
+        <v-btn @click="$refs.calendar.prev()">
+          <v-icon dark left>
+            keyboard_arrow_left
+          </v-icon>
+          Prev
+        </v-btn>
+
+        <h2>{{ date }}</h2>
+
+        <v-btn @click="$refs.calendar.next()">
+          Next
+          <v-icon right dark>
+            keyboard_arrow_right
+          </v-icon>
+        </v-btn>
+      </div>
+      <v-sheet height="500">
+        <v-calendar
+          ref="calendar"
+          v-model="start"
+          :type="type"
+          color="primary"
+        ></v-calendar>
+      </v-sheet>
+    </div>
   </div>
 </template>
-<script src="./Calendar.js">
-</script>
-<style lang="scss" scoped src="./Calendar.scss">
-</style>
+<script src="./Calendar.js"></script>
+<style lang="scss" scoped src="./Calendar.scss"></style>
