23790c54cbde8902d7c0b9bf8efd98f4d2bc26bc
Roland doda <rolanddoda2014@gmail.com>
2019-02-15 17:04:16 +0000

    Merged in bugfix/console-error-when-changing-route (pull request #565)
    Fix error when changing route

--- src/common/TableHeader.vue
+++ src/common/TableHeader.vue
@@ -4,9 +4,7 @@
     <v-btn v-if="!noButton" fab small class="add__btn" @click="$emit('click')">
       <v-icon>add</v-icon>
     </v-btn>
-    <slot name="form-btn">
-      {{ content }}
-    </slot>
+    <slot name="form-btn" />
   </v-layout>
 </template>
 
