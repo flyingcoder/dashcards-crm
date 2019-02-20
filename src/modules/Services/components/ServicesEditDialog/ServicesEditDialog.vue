77342d5675d81be6e1d5317752d1d08e36fa4c95
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-02-20 17:12:09 +0000

    Merged in suitable-rightway/design-edit-service-dialog (pull request #572)
    Design edit service dialog
    
    Approved-by: Mohammad Alongan <mohammad.buzzooka@gmail.com>

--- src/modules/Services/components/ServicesEditDialog/ServicesEditDialog.vue
+++ src/modules/Services/components/ServicesEditDialog/ServicesEditDialog.vue
@@ -1,83 +1,31 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="open" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{ title }}</span>
-        </v-card-title>
-
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout row wrap>
-              <v-flex xs10>
-                <v-text-field
-                  label="Service name"
-                  v-model.trim="name"
-                  @keyup.enter="add_service"
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="cancel">Cancel</v-btn>
-          <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+  <div class="edit-service">
+    <CustomDialog
+      title="Edit Service"
+      ref="dialog"
+      :open.sync="open"
+      button2-text="Save"
+      @button1="open = false"
+      @button2="save"
+    >
+      <template slot="content">
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-text-field
+              class="service__input"
+              label="Service name"
+              v-model.trim="name"
+              @keyup.enter="add_service"
+              solo
+              hide-details
+              color="#657186"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+      </template>
+    </CustomDialog>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'ServicesEditDialog',
-
-  props: {
-    dialog: Boolean,
-    title: String,
-    isEditDialog: Boolean,
-    fieldsToEdit: { type: Object, default: () => {} }
-  },
-
-  data: () => ({
-    open: false,
-    name: null
-  }),
-
-  watch: {
-    dialog(new_val) {
-      this.open = new_val
-    },
-    open(new_val) {
-      this.$emit('update:dialog', new_val)
-    },
-    fieldsToEdit: {
-      handler(new_val) {
-        this.isEditDialog && this.update_fields(new_val)
-      },
-      deep: true
-    }
-  },
-
-  methods: {
-    cancel() {
-      this.open = false
-    },
-    save() {
-      const fields_to_save = {
-        name: this.name
-      }
-      this.$emit('save', fields_to_save)
-    },
-
-    update_fields({ fields }) {
-      const new_fields = Object.assign({}, fields)
-      this.name = new_fields.service_name
-    }
-  }
-}
+<script src="./ServicesEditDialog.js">
 </script>
-
-<style scoped></style>
