766e0441dd2ebf9b6db03886a177d7fa984f1455
Roland doda <rolanddoda2014@gmail.com>
2019-03-31 18:37:42 +0000

    Merged in feature/invoice-tab (pull request #650)
    Finish invoice tab table

---
+++ src/modules/ProjectPreview-Invoice/Invoice.js
@@ -0,0 +1,35 @@
+import * as apiTo from './api'
+import { table_functionality } from '@/services/table-functionality/table-functionality'
+//Components
+import CustomTable from '@/common/CustomTable/CustomTable.vue'
+
+export default {
+  name: 'Invoice',
+  mixins: [table_functionality],
+
+  components: {
+    CustomTable
+  },
+
+  props: {
+    id: [Number, String]
+  },
+
+  data: () => ({
+    headers: [
+      { id: 1, text: 'Invoice', value: 'invoice' },
+      { id: 2, text: 'Due Date', value: 'due_date' },
+      { id: 3, text: 'Client', value: 'client' },
+      { id: 4, text: 'Amount', value: 'amount' },
+      { id: 5, is_action: false }
+    ]
+  }),
+
+  created() {
+    this.loading = true
+    apiTo
+      .getInvoices(this.id)
+      .then(({ data }) => this.add_table_rows(data.data, data))
+      .finally(() => (this.loading = false))
+  }
+}
--- src/modules/ProjectPreview-Invoice/Invoice.vue
+++ src/modules/ProjectPreview-Invoice/Invoice.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="invoice">
-    <Invoice />
+    <custom-table
+      v-if="items.length || loading"
+      :headers="headers"
+      :items="items"
+      :loading="loading"
+      :sort="sort"
+      :permission="$_permissions.get('invoice')"
+      no-row-edit
+      no-row-view
+      no-row-delete
+      toolbar-title="Invoice"
+      hide-actions
+      @items-selected="selected_ids = $event"
+    >
+      <template slot="custom-item" slot-scope="{ item }">
+        <td>{{ item.title }}</td>
+        <td>{{ item.due_date }}</td>
+        <td>{{ item.billed_to }}</td>
+        <td>{{ item.total_amount }}</td>
+      </template>
+    </custom-table>
+
+    <div class="empty-invoice" v-else>
+      <div class="empty-content">
+        <div class="empty-svg">
+          <svg viewBox="0 0 250 250">
+            <path
+              d="M37 11l176 0c11,0 20,4 26,11 7,6 11,16 11,26l0 154c0,10 -4,19 -11,26 -2,2 -4,3 -6,5 -17,13 -33,1 -48,-11 -10,-7 -19,-14 -26,-11 -5,2 -9,7 -12,13 -12,18 -31,17 -46,4 -5,-5 -11,-10 -16,-11 -6,-1 -12,4 -19,9 -11,8 -23,17 -39,11 -6,-1 -12,-5 -16,-9 -7,-7 -11,-16 -11,-26l0 -154c0,-10 4,-20 11,-26 6,-7 15,-11 26,-11zm22 108c8,0 15,7 15,15 0,8 -7,15 -15,15 -8,0 -15,-7 -15,-15 0,-8 7,-15 15,-15zm0 -56c8,0 15,7 15,15 0,8 -7,15 -15,15 -8,0 -15,-7 -15,-15 0,-8 7,-15 15,-15zm44 24c-4,0 -7,-4 -7,-8 0,-4 3,-7 7,-7l96 0c4,0 7,3 7,7 0,4 -3,8 -7,8l-96 0zm0 56c-4,0 -7,-4 -7,-8 0,-4 3,-7 7,-7l96 0c4,0 7,3 7,7 0,4 -3,8 -7,8l-96 0zm129 70c2,-3 3,-7 3,-11l0 -154c0,-6 -2,-12 -6,-16 -4,-4 -10,-6 -16,-6l-176 0c-6,0 -12,2 -16,6 -4,4 -6,10 -6,16l0 154c0,6 2,11 6,15 1,2 3,3 4,3 13,8 23,1 32,-6 9,-7 18,-13 30,-12 10,2 17,8 24,15 8,7 17,8 24,-1 4,-8 9,-15 19,-19 14,-6 27,3 39,13 13,10 26,20 39,3z"
+            />
+          </svg>
+        </div>
+        <h2>No Invoice yet</h2>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import Invoice from '@/modules/Invoice/Invoice.vue'
-export default {
-  components: {
-    Invoice
-  },
-  beforeDestroy() {
-    this.$store.commit('invoice/reset_state')
-  }
+<script src="./Invoice.js"></script>
+<style lang="scss" scoped src="./Invoice.scss"></style>
+<style scoped>
+>>> .table__toolbar .v-toolbar__content {
+  all: unset;
 }
-</script>
+</style>
