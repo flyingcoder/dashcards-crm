291c45bf39e2e60ef2887b2fe6a2c77f867c8233
Mohammad Alongan <mohammad.buzzooka@gmail.com>
2019-04-10 17:26:56 +0000

    Merged in feature-rightway/add-copy-permission-dropdown-field-in-add-group-dialog (pull request #682)
    Add Copy Permission dropdown field in add group dialog
    
    Approved-by: Mohammad Alongan <mohammad.buzzooka@gmail.com>

--- src/modules/Settings/components/Groups/components/GroupsDialog/GroupsDialog.js
+++ src/modules/Settings/components/Groups/components/GroupsDialog/GroupsDialog.js
@@ -1,7 +1,12 @@
 import cloneDeep from 'lodash/cloneDeep'
+import request from '@/services/axios_instance'
+import CustomDialog from '@/common/BaseComponents/CustomDialog/CustomDialog.vue'
 
 export default {
   name: 'GroupsDialog',
+  components: {
+    CustomDialog
+  },
 
   props: {
     dialog: Boolean,
@@ -14,25 +19,32 @@ export default {
     open: false,
     name: '',
     description: '',
-    permission_items: [],
-    selected_permission: null
+    selected_group: null,
+    group_items: []
   }),
 
   computed: {
     are_fields_valid() {
-      return !!(this.name && this.description) && this.does_something_changed
+      return !!(this.name && this.selected_group) && this.does_something_changed
     },
     does_something_changed() {
       if (!this.isEditDialog) return true //if it is not Edit Dialog
       return (
         this.name !== this.fieldsToEdit.fields.name ||
-        this.description !== this.fieldsToEdit.fields.description
+        this.description !== this.fieldsToEdit.fields.description ||
+        this.selected_group !== this.fieldsToEdit.fields.selected_group
       )
     }
   },
 
   watch: {
     dialog(new_val) {
+      if (new_val) {
+        //when dialog opens
+        request
+          .get(`api/groups?all=true`)
+          .then(({ data }) => (this.group_items = data))
+      }
       this.open = new_val
     },
     open(new_val) {
@@ -52,13 +64,16 @@ export default {
     },
     save() {
       if (this.validation_passed()) {
-        let name = this.name
-        let description = this.description
-        this.$emit('save', { name, description })
+        const fields_to_save = {
+          name: this.name,
+          description: this.description,
+          selected_group: this.selected_group
+        }
+        this.$emit('save', fields_to_save)
       }
     },
     validation_passed() {
-      if (this.name && this.description && this.does_something_changed) {
+      if (this.name && this.selected_group && this.does_something_changed) {
         return true
       }
       this.$event.$emit('open_snackbar', 'Nothing Changed!', 'notification')
@@ -68,11 +83,11 @@ export default {
       const new_fields = cloneDeep(fields)
       this.name = new_fields.name
       this.description = new_fields.description
+      this.selected_group = new_fields.selected_group
     },
 
     clear_and_close() {
-      this.name = ''
-      this.description = ''
+      Object.assign(this.$data, this.$options.data.apply(this))
       this.cancel() //close the modal
     }
   }
--- src/modules/Settings/components/Groups/components/GroupsDialog/GroupsDialog.vue
+++ src/modules/Settings/components/Groups/components/GroupsDialog/GroupsDialog.vue
@@ -1,48 +1,54 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="open" persistent max-width="600px">
-      <v-card class="groups-dialog">
-        <v-card-title class="dialog__header">
-          <span class="dialog__title">{{ title }}</span>
-          <v-btn fab small class="close__dialog" @click="cancel">
-            <v-icon>close</v-icon>
-          </v-btn>
-        </v-card-title>
 
-        <v-card-text class="dialog__body">
-          <v-layout wrap>
-            <v-flex xs12>
-              <v-text-field
-                class="dialog__textfield"
-                solo
-                hide-details
-                color="#657186"
-                v-model.trim="name"
-                label="Name"
-              />
-            </v-flex>
+  
+  <custom-dialog
+    :title="title"
+    ref="dialog"
+    :open.sync="open"
+    button2-text="Save"
+    @button1="cancel"
+    @button2="save"
+  >
+    <template #content>
+      <v-layout wrap>
+          <v-flex xs12>
+            <v-text-field
+              class="dialog__textfield d-field"
+              box
+              hide-details
+              color="#657186"
+              v-model.trim="name"
+              label="Name"
+            />
+          </v-flex>
 
-            <v-flex xs12>
-              <v-textarea
-                class="dialog__textarea"
-                solo
-                hide-details
-                color="#657186"
-                label="Description"
-                v-model.trim="description"
-              />
-            </v-flex>
-          </v-layout>
-        </v-card-text>
+          <v-flex xs12>
+            <v-select
+              class="dialog__selectfield d-field"
+              box
+              hide-details
+              color="#657186"
+              label="Copy Permission"
+              v-model.trim="selected_group"
+              :items="group_items"
+              item-text="name"
+              item-value="id"
+            />
+          </v-flex>
 
-        <v-card-actions class="dialog__actions">
-          <v-btn @click="cancel">Cancel</v-btn>
-          <v-btn @click="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+          <v-flex xs12>
+            <v-textarea
+              class="dialog__textarea d-field"
+              box
+              hide-details
+              color="#657186"
+              label="Description"
+              v-model.trim="description"
+            />
+          </v-flex>
+        </v-layout>
+    </template>
+  </custom-dialog>
 </template>
 <script src="./GroupsDialog.js"></script>
 <style lang="scss" scoped src="./GroupsDialog.scss"></style>
-<style scoped src="./GroupsDialog.css"></style>
