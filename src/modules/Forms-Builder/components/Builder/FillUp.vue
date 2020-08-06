76431a3025b411a238a8c78317afebbc464fa56f
kirbycaps <kcgerona@gmail.com>
2020-08-07 00:58:25 +0800

    improve form builder added sticky sidebar

---
+++ src/modules/Forms-Builder/components/Builder/FillUp.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "FillUp"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/modules/Reports/components/ReportAddViaTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ReportAddViaTemplate"
+    }
+</script>
+
+<style scoped>
+
+</style>
